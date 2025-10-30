8b4d74eeac9bdf9f828efd6e5ec508852a33179b
ivensdev <ivensdeveloper@gmail.com>
2025-10-30 14:36:00 +0000

    Fix auth update user

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -3,12 +3,17 @@ import simpleRestProvider from 'ra-data-simple-rest';
 import { API_URL } from './config/api';
 
 const httpClient = (url: string, options: fetchUtils.Options = {}) => {
-    const token = localStorage.getItem('auth_token');
-    options.user = {
-        authenticated: true,
-        token: `Bearer ${token}`,
+    // const token = localStorage.getItem('auth_token');
+    // options.user = {
+    //     authenticated: true,
+    //     token: `Bearer ${token}`,
+    // };
+    // return fetchUtils.fetchJson(url, options);
+    const fetchOptions: fetchUtils.Options = {
+        ...options,
+        credentials: 'include',
     };
-    return fetchUtils.fetchJson(url, options);
+    return fetchUtils.fetchJson(url, fetchOptions);
 };
 
 const dataProvider = simpleRestProvider(API_URL, httpClient);
